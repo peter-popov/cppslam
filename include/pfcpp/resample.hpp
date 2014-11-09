b84154fa5effad1b765b376d98e2f13b0d2af749
Peter Popov <petro.popov@gmail.com>
2014-11-09 11:59:34 +0100

    resampling unit-tests

--- include/pfcpp/resample.hpp
+++ include/pfcpp/resample.hpp
@@ -19,7 +19,7 @@ void resample(const std::vector<P>& in, std::vector<P>& out, Sampler sampler)
 	if (out.size() != in.size())
 		out.resize(in.size());
 
-	MultimodalDistribution<double> mmdist(in.begin(), in.end(), [](const auto& x){return get_weight(x);});
+	MultimodalDistribution<double> mmdist(in.begin(), in.end(), [](const auto& x){ return get_weight(x); });
 	  
  	for (int i = 0; i < in.size(); ++i)
 		out[i] = in[mmdist.draw(sampler(i))];
@@ -29,32 +29,27 @@ void resample(const std::vector<P>& in, std::vector<P>& out, Sampler sampler)
 /**
 * Multimodal resampling
 */
-template<typename P>
-void multimodal_resample(const std::vector<P>& in, std::vector<P>& out)
-{
-	Uniform<double> unif(0, 1.0);
-	resample(in, out, [&](auto idx){ return unif();});
+template<typename P, typename Gen = Uniform<double>>
+void multimodal_resample(const std::vector<P>& in, std::vector<P>& out, Gen gen = Gen(0, 1.0)) {
+	resample(in, out, [&](auto idx) { return gen(); });
 }
 
 
 /**
 *
 */
-template<typename P>
-void stratified_resample(const std::vector<P>& in, std::vector<P>& out)
-{
-	Uniform<double> unif(0, 1.0);
-	resample(in, out, [&](auto idx){ return ((idx - 1) + unif()) / in.size();});
+template<typename P, typename Gen = Uniform<double>>
+void stratified_resample(const std::vector<P>& in, std::vector<P>& out, Gen gen = Gen(0, 1.0)) {
+	resample(in, out, [&](auto idx) { return (idx + gen()) / in.size(); });
 }
+
 
 /**
 *
 */
-template<typename P>
-void systematic_resample(const std::vector<P>& in, std::vector<P>& out)
-{
-	Uniform<double> unif(0, 1.0);	
-	resample(in, out, [&, sample = unif()] (auto idx) {return ((idx - 1) + sample) / in.size();});
+template<typename P, typename Gen = Uniform<double>>
+void systematic_resample(const std::vector<P>& in, std::vector<P>& out, Gen gen = Gen(0, 1.0)) {
+	resample(in, out, [&, sample = gen()](auto idx) { return (idx + sample) / in.size(); });
 }
 
 } //pfcpp
--- test/resample_test.cpp
+++ test/resample_test.cpp
@@ -1,35 +1,112 @@
 #include <pfcpp/resample.hpp>
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(resampling)
-
-BOOST_AUTO_TEST_CASE( test_resample_weight_member )
+//
+// Mock random number generator
+template<typename T>
+struct MockGenerator
 {
-	struct P
+	MockGenerator(std::initializer_list<T> v)
+	: numbers(v) {
+	}
+
+	T operator()()
 	{
-		int id;
-		double weight;
-	};
+		return numbers[i++ % numbers.size()];
+	}
 
-	std::vector<P> particles{ {0, 0.1}, {1, 0.2}, {2, 0.7} };    	
-	std::vector<P> resampled;
-	pfcpp::multimodal_resample(particles, resampled);
-}
+	size_t i = 0;
+	std::vector<T> numbers;
+};
 
 
-struct MyP
+struct TestParticle
+{
+	int id;
+	double weight;
+};
+
+struct CustomTestParticle
 {
 	int id;
 	double other_field;
 };
 
-auto get_weight(const MyP& p) {return p.other_field;};
+auto get_weight(const CustomTestParticle& p) {return p.other_field;};
 
-BOOST_AUTO_TEST_CASE( test_resample_custom )
+
+//=================================================================================================
+// Unit-test for resampling functionality
+//=================================================================================================
+BOOST_AUTO_TEST_SUITE(resampling)
+
+//
+//
+BOOST_AUTO_TEST_CASE( test_resample_compiles_with_custom_particle_class )
 {
-	std::vector<MyP> particles{ {0, 0.1}, {1, 0.2}, {2, 0.7} };    	
-	std::vector<MyP> resampled;
+	std::vector<CustomTestParticle> particles{ {0, 0.1}, {1, 0.2}, {2, 0.7} };    	
+	std::vector<CustomTestParticle> resampled;
 	pfcpp::multimodal_resample(particles, resampled);	
 }
+
+//
+//
+BOOST_AUTO_TEST_CASE( test_mock_generator )
+{
+	auto gen = MockGenerator<double>{0.1, 0.2, 0.3};
+
+	BOOST_CHECK_CLOSE(0.1, gen(), 0.00001);
+	BOOST_CHECK_CLOSE(0.2, gen(), 0.00001);
+	BOOST_CHECK_CLOSE(0.3, gen(), 0.00001);
+	BOOST_CHECK_CLOSE(0.1, gen(), 0.00001);
+}
+
+//
+//
+BOOST_AUTO_TEST_CASE( test_resample_multimodal )
+{
+	auto gen = MockGenerator<double>{0.1, 0.1, 0.9};
+
+	std::vector<TestParticle> particles{ {0, 0.2}, {1, 0.2}, {2, 0.6} };    	
+	std::vector<TestParticle> resampled;
+	pfcpp::multimodal_resample(particles, resampled, gen);
+
+	BOOST_CHECK_EQUAL(0, resampled[0].id);
+	BOOST_CHECK_EQUAL(0, resampled[1].id);
+	BOOST_CHECK_EQUAL(2, resampled[2].id);
+}
+
+//
+//
+BOOST_AUTO_TEST_CASE( test_resample_stratified )
+{
+	auto gen = MockGenerator<double>{0.1, 0.1, 0.9};
+
+
+	std::vector<TestParticle> particles{ {0, 0.2}, {1, 0.6}, {2, 0.2} };    	
+	std::vector<TestParticle> resampled;
+	pfcpp::stratified_resample(particles, resampled, gen);
+
+	BOOST_CHECK_EQUAL(0, resampled[0].id);
+	BOOST_CHECK_EQUAL(1, resampled[1].id);
+	BOOST_CHECK_EQUAL(2, resampled[2].id);
+}
+
+//
+//
+BOOST_AUTO_TEST_CASE( test_resample_systematic )
+{
+	auto gen = MockGenerator<double>{0.1, 0.1, 0.9};
+
+
+	std::vector<TestParticle> particles{ {0, 0.2}, {1, 0.6}, {2, 0.2} };    	
+	std::vector<TestParticle> resampled;
+	pfcpp::systematic_resample(particles, resampled, gen);
+
+	BOOST_CHECK_EQUAL(0, resampled[0].id);
+	BOOST_CHECK_EQUAL(1, resampled[1].id);
+	BOOST_CHECK_EQUAL(1, resampled[2].id);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
